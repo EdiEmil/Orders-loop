2322611cd10aca79e7d5766377d1a3771ce4ecf5
EdiEmil <eduardemil10@yahoo.com>
2025-07-14 12:03:35 +0300

    test change

--- src/test/java/com/order/tracker/order/tracker/service/ApplicationTests.java
+++ src/test/java/com/order/tracker/order/tracker/service/ApplicationTests.java
@@ -3,9 +3,10 @@ package com.order.tracker.order.tracker.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@DataJpaTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
